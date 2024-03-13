fa50b45d4345c156001395d59e5d845dc6eff2e1
Kaseriox <88597455+Kaseriox@users.noreply.github.com>
2024-03-13 21:13:37 +0200

    ID-42 PocketBase Configuration (#4)
    * ID-42 add server directory with gitignore
    
    * ID-42 update gitignore to ignore data pocketbase data folder
    
    * ID-42 added readme how to configure pocketbase
    
    * ID-42 configured pocketbase migrations
    
    ---------
    
    Co-authored-by: Kaseriox <>

---
+++ server/pb_migrations/1710325820_updated_users.js
@@ -0,0 +1,82 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  collection.options = {
+    "allowEmailAuth": true,
+    "allowOAuth2Auth": false,
+    "allowUsernameAuth": false,
+    "exceptEmailDomains": null,
+    "manageRule": null,
+    "minPasswordLength": 8,
+    "onlyEmailDomains": null,
+    "onlyVerified": false,
+    "requireEmail": false
+  }
+
+  // remove
+  collection.schema.removeField("users_name")
+
+  // remove
+  collection.schema.removeField("users_avatar")
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  collection.options = {
+    "allowEmailAuth": true,
+    "allowOAuth2Auth": true,
+    "allowUsernameAuth": true,
+    "exceptEmailDomains": null,
+    "manageRule": null,
+    "minPasswordLength": 8,
+    "onlyEmailDomains": null,
+    "onlyVerified": false,
+    "requireEmail": false
+  }
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "users_name",
+    "name": "name",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "users_avatar",
+    "name": "avatar",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [
+        "image/jpeg",
+        "image/png",
+        "image/svg+xml",
+        "image/gif",
+        "image/webp"
+      ],
+      "thumbs": null,
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
