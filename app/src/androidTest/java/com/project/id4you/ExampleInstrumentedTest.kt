8519cdb2c2b01f7e796739a97b1f87d880657966
Dovydas Šablevičius <dovydas.sablevicius.it@gmail.com>
2024-03-14 12:54:47 +0200

    ID-49 test test pipeline

--- app/src/androidTest/java/com/project/id4you/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/project/id4you/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.project.id4youui", appContext.packageName)
+        assertEquals("com.project.id4you", appContext.packageName)
     }
 }
