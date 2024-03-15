35f86743850195c3970e5660e77f515656ad0b9c
Eimantas <eimantas.dziovenas@gmail.com>
2024-03-15 21:40:40 +0200

    ID-54 add hide/show functions

--- app/src/main/java/com/project/id4you/presentation/MainActivity.kt
+++ app/src/main/java/com/project/id4you/presentation/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.project.id4you.presentation.components.TextField
 import com.project.id4you.presentation.ui.theme.ID4YouTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,8 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //Greeting("Android")
-                    TextField(labelText = "Enter email")
+                    Greeting("Android")
                 }
             }
         }
--- app/src/main/java/com/project/id4you/presentation/components/TextField.kt
+++ app/src/main/java/com/project/id4you/presentation/components/TextField.kt
@@ -1,48 +1,73 @@
 package com.project.id4you.presentation.components
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.SystemFontFamily
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.project.id4you.presentation.ui.theme.Blue
 
 
 @Composable
 fun TextField(
     labelText: String,
+    isPasswordField: Boolean = false,
     color: Color = Blue,
     borderWidth: Dp = 1.dp,
-    method: () -> Unit = {},
+    fontFamily: SystemFontFamily = FontFamily.Default,
+    fontStyle: FontStyle = FontStyle.Normal,
+    fontSize: TextUnit = 12.sp,
     fontWeight: Int = 500,
-    passedModifier: Modifier = Modifier
-        .background(color = Color.Transparent)
-        .fillMaxWidth() // Make the TextField full width
-        .padding(horizontal = 20.dp, vertical = 24.dp)
-        .border(
-            BorderStroke(borderWidth, color), // Blue border with 1dp width
-            shape = RoundedCornerShape(8.dp)
-        ),
+    horizontalPadding: Dp = 20.dp,
+    verticalPadding: Dp = 10.dp,
+    roundCornerRadius: Dp = 8.dp,
 ) {
-    Column {
+    val (isPasswordVisible, setPasswordVisible) = remember { mutableStateOf(!isPasswordField) }
+    val (inputValue, setInputValue) = remember { mutableStateOf(TextFieldValue()) }
+
+    Box() {
         TextField(
-            value = "Text",
-            onValueChange = { method },
+            value = inputValue,
+            onValueChange = { newValue -> setInputValue(newValue) },
             label = {
-                Text("$labelText", fontWeight = FontWeight(fontWeight))
+                Text(
+                    text = labelText,
+                    fontFamily = fontFamily,
+                    fontStyle = fontStyle,
+                    fontWeight = FontWeight(fontWeight),
+                    fontSize = fontSize
+                )
             },
-            modifier = passedModifier,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontalPadding, verticalPadding)
+                .border(
+                    BorderStroke(borderWidth, color),
+                    shape = RoundedCornerShape(roundCornerRadius)
+                ),
             colors = TextFieldDefaults.colors(
                 focusedContainerColor = Color.Transparent,
                 unfocusedContainerColor = Color.Transparent,
@@ -52,7 +77,29 @@ fun TextField(
                 disabledIndicatorColor = Color.Transparent, // No disabled underline color
                 focusedLabelColor = color,
                 unfocusedLabelColor = color,
-            )
+            ),
+            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
         )
+        if (isPasswordField) {
+            Button(
+                onClick = { setPasswordVisible(!isPasswordVisible) },
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = color,
+                    containerColor = Color.Transparent
+                ),
+                modifier = Modifier
+                    .padding(start = 4.dp, top = 15.dp, end = 24.dp)
+                    .align(Alignment.TopEnd)
+                //.offset(x = (300).dp, y = 20.dp)
+            ) {
+                Text(
+                    text = if (isPasswordVisible) "Hide" else "Show",
+                    fontFamily = fontFamily,
+                    fontStyle = fontStyle,
+                    fontWeight = FontWeight(fontWeight),
+                    fontSize = fontSize
+                )
+            }
+        }
     }
 }
