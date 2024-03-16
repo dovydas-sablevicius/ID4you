d0361db5b0fe3b281987240bb46b2d7c200f6e88
Kaseriox <>
2024-03-16 16:07:40 +0200

    ID-61 implement User registration and login in PocketBaseApi.kt

--- app/src/main/java/com/project/id4you/data/remote/PocketBaseApi.kt
+++ app/src/main/java/com/project/id4you/data/remote/PocketBaseApi.kt
@@ -1,6 +1,16 @@
 package com.project.id4you.data.remote
 
-@Suppress("EmptyClassBlock") //Remove when implementing the class
+import com.project.id4you.data.remote.dto.UserDto
+import com.project.id4you.data.remote.dto.UserLoginDto
+import com.project.id4you.data.remote.dto.UserRegistrationDto
+import retrofit2.http.Body
+import retrofit2.http.POST
 
 interface PocketBaseApi {
+    @POST("/collections/users/records")
+    suspend fun registerUser(@Body userRegistrationDto: UserRegistrationDto): Unit
+
+    @POST("/collections/users/auth-with-password")
+    suspend fun loginUser(@Body userLoginDto: UserLoginDto): UserDto
+
 }
