f28576e173a7732388e6445e377be2e711d21d16
Kaseriox <>
2024-03-16 16:05:37 +0200

    ID-61 add PocketBase base url to Constants.kt

--- app/src/main/java/com/project/id4you/common/Constants.kt
+++ app/src/main/java/com/project/id4you/common/Constants.kt
@@ -3,4 +3,5 @@ package com.project.id4you.common
 @Suppress("EmptyClassBlock") //Remove when implementing the class
 
 object Constants {
+    val BASE_URL: String = "http://127.0.0.1:8090/api/";
 }
