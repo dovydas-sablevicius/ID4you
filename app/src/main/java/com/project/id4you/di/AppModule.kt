65145faf4e22057ad70f7441944a344f4e5d797c
Kaseriox <>
2024-03-16 19:21:26 +0200

    ID-61 added AppModule.kt

--- app/src/main/java/com/project/id4you/di/AppModule.kt
+++ app/src/main/java/com/project/id4you/di/AppModule.kt
@@ -1,5 +1,33 @@
 package com.project.id4you.di
 
-@Suppress("EmptyClassBlock") //Remove when implementing the class
+import com.project.id4you.common.Constants
+import com.project.id4you.data.remote.PocketBaseApi
+import com.project.id4you.data.repository.UserRepositoryImpl
+import com.project.id4you.domain.repository.UserRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+    @Provides
+    @Singleton
+    fun providePocketBaseApi(): PocketBaseApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(PocketBaseApi::class.java);
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserRepository(api: PocketBaseApi): UserRepository {
+        return UserRepositoryImpl(api);
+    }
 }
