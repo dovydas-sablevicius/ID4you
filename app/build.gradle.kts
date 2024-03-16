e697e4f37e0ad59946d3adb484b613ad6ac97c16
Eimantas Džiovėnas <115113879+3imantas@users.noreply.github.com>
2024-03-16 17:56:54 +0200

    ID-57 icon button components (#13)
    * a
    
    * ID-57 add icon button components
    
    * ID-57 fix newline
    
    * ID-57 fix gradlew.bat newlines
    
    * ID-57 Delete .idea directory
    
    * ID-57 renamed components

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("com.google.android.material:material:1.2.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
