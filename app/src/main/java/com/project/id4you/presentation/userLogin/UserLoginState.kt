6d215849109994877e78d62dd5db785443a00b95
Kaseriox <>
2024-03-16 20:30:44 +0200

    ID-61 implemented UserLoginState and UserLoginViewModel

--- app/src/main/java/com/project/id4you/presentation/userLogin/UserLoginState.kt
+++ app/src/main/java/com/project/id4you/presentation/userLogin/UserLoginState.kt
@@ -4,8 +4,6 @@ import com.project.id4you.data.repository.model.User
 
 data class UserLoginState(
     val isLoading: Boolean = false,
-    val email: String = "",
-    val password: String = "",
     val error: String = "",
     val user: User? = null
 )
