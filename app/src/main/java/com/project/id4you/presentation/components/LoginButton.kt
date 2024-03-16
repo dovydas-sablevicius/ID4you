2b7f65c3ceb211f77dec66f8ab023bcb7fc955ea
Martinas Pilecki <martinas.pilecki@stud.vilniustech.lt>
2024-03-16 15:49:17 +0200

    ID-53 fixed errors

--- app/src/main/java/com/project/id4you/presentation/components/LoginButton.kt
+++ app/src/main/java/com/project/id4you/presentation/components/LoginButton.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
+@Suppress("LongParameterList")
 @Composable
 fun loginButton(
     labelText: String,
