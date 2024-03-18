40532b528db692a8beae5e37646fe1a0f5066704
Kaseriox <>
2024-03-18 19:25:25 +0200

    ID-45 add Exception Messages file

---
+++ app/src/main/java/com/project/id4you/common/ExceptionMessages.kt
@@ -0,0 +1,6 @@
+package com.project.id4you.common
+
+object ExceptionMessages {
+    const val httpExceptionMessage: String = "An unexpected error occurred."
+    const val ioExceptionMessage: String = "Couldn't reach server."
+}
