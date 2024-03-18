13924fdab045a6f243c7fe95c5f05513787c0015
Eimantas <eimantas.dziovenas@gmail.com>
2024-03-19 00:13:52 +0200

    ID-29 fix newline and empty block

--- app/src/main/java/com/project/id4you/presentation/DocumentPageScreen.kt
+++ app/src/main/java/com/project/id4you/presentation/DocumentPageScreen.kt
@@ -1,11 +1,16 @@
 package com.project.id4you.presentation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
+import androidx.compose.ui.unit.sp
+import com.project.id4you.presentation.components.TextComponent
 
 @Composable
 fun UserDocumentPageScreen(
-    navController: NavController,
+    //navController: NavController,
 ) {
-
+    TextComponent(
+        labelText = "Documents page",
+        fontWeight = 500,
+        fontSize = 32.sp
+    )
 }
