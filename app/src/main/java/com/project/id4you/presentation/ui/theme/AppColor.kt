88809823028159785df002c83526937f7cb377fe
Kaseriox <88597455+Kaseriox@users.noreply.github.com>
2024-03-18 18:22:37 +0200

    ID-63 Renamed Color.kt file to AppColor.kt and changed class name from Color to AppColor (#21)
    Co-authored-by: Kaseriox <>

--- app/src/main/java/com/project/id4you/presentation/ui/theme/AppColor.kt
+++ app/src/main/java/com/project/id4you/presentation/ui/theme/AppColor.kt
@@ -3,7 +3,7 @@ package com.project.id4you.presentation.ui.theme
 import androidx.compose.ui.graphics.Color
 
 @Suppress("UtilityClassWithPublicConstructor")
-class Color {
+class AppColor {
     companion object {
         val Black = Color(0xFF000000)
         val White = Color(0xFFFFFFFF)
