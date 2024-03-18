cc2a28b63b17a1db81f7d6ef642ed5f03e200a27
Eimantas <eimantas.dziovenas@gmail.com>
2024-03-18 23:58:45 +0200

    ID-29 refactor ErrorText and LoadingIndicator components

---
+++ app/src/main/java/com/project/id4you/presentation/DocumentPageScreen.kt
@@ -0,0 +1,11 @@
+package com.project.id4you.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun UserDocumentPageScreen(
+    navController: NavController,
+) {
+
+}
