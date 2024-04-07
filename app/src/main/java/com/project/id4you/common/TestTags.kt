01f5b4880fae0a4d23cf6a0fbcaa51f64253e3c2
Eimantas <eimantas.dziovenas@gmail.com>
2024-04-07 23:57:29 +0300

    ID-71 add registration screen test

--- app/src/main/java/com/project/id4you/common/TestTags.kt
+++ app/src/main/java/com/project/id4you/common/TestTags.kt
@@ -1,10 +1,16 @@
 package com.project.id4you.common
 
 object TestTags {
+    const val NAVIGATE_TO_LOGIN_BUTTON = "NavigateToLogin"
     const val NAVIGATE_TO_REGISTRATION_BUTTON = "NavigateToRegistration"
     const val REGISTRATION_SCREEN = "RegistrationScreen"
+    const val LOGIN_SCREEN = "LoginScreen"
     const val LOGIN_SCREEN_EMAIL_INPUT = "LoginEmailInput"
     const val LOGIN_SCREEN_PASSWORD_INPUT = "LoginPasswordInput"
+    const val REGISTRATION_SCREEN_EMAIL_INPUT = "RegistrationEmailInput"
+    const val REGISTRATION_SCREEN_PASSWORD_INPUT = "RegistrationPasswordInput"
+    const val REGISTRATION_SCREEN_PASSWORD_CONFIRM_INPUT = "RegistrationPasswordConfirmInput"
+    const val SIGN_UP_BUTTON = "SignUpButton"
     const val LOGIN_BUTTON = "LoginButton"
     const val DOCUMENT_LIST_SCREEN = "DocumentListScreen"
     const val FORGOT_PASSWORD_BUTTON = "ForgotPasswordButton"
