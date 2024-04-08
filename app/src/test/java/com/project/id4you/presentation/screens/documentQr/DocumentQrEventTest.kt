2d21aefd759f5cff651bad6e334eca1a7a13b16e
Dovydas Šablevičius <dovydas.sablevicius.it@gmail.com>
2024-04-08 19:47:49 +0300

    ID-22 add test files

---
+++ app/src/test/java/com/project/id4you/presentation/screens/documentQr/DocumentQrEventTest.kt
@@ -0,0 +1,10 @@
+package com.project.id4you.presentation.screens.documentQr
+
+import org.junit.Test
+
+class DocumentQrEventTest {
+    @Test
+    fun testRegenerateQrCodeEvent() {
+        DocumentQrEvent.RegenerateQrCode
+    }
+}
