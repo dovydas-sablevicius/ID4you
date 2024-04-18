5b97da55e75b34c579ea3f7d652542143bf2129f
Kaseriox <88597455+Kaseriox@users.noreply.github.com>
2024-04-18 14:21:05 +0300

    ID-26 Update PocketBase Migrations

---
+++ server/pb_migrations/1713438946_updated_Document.js
@@ -0,0 +1,218 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("q706vkto9k1axv6")
+
+  collection.listRule = "@request.auth.id = document_owner.id"
+  collection.viewRule = "@request.auth.id = document_owner.id"
+  collection.createRule = "@request.data.valid:isset = false && @request.auth.id = @request.data.document_owner.id"
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "bzqxxvb6",
+    "name": "type",
+    "type": "select",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "values": [
+        "Identity Card",
+        "Passport",
+        "Student Card"
+      ]
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "my93my0q",
+    "name": "document_owner",
+    "type": "relation",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "_pb_users_auth_",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "gr43t3vm",
+    "name": "document_name",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "i3x66qyy",
+    "name": "passport_code",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ghk3ht54",
+    "name": "valid",
+    "type": "bool",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {}
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "w3m2jabv",
+    "name": "document_photos",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [],
+      "thumbs": [],
+      "maxSelect": 99,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("q706vkto9k1axv6")
+
+  collection.listRule = "@request.auth.id = Document_Owner.id"
+  collection.viewRule = "@request.auth.id = Document_Owner.id"
+  collection.createRule = "@request.data.Valid:isset = false && @request.auth.id = @request.data.Document_Owner.id"
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "bzqxxvb6",
+    "name": "Type",
+    "type": "select",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "values": [
+        "Identity Card",
+        "Passport",
+        "Student Card"
+      ]
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "my93my0q",
+    "name": "Document_Owner",
+    "type": "relation",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "_pb_users_auth_",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "gr43t3vm",
+    "name": "Document_Name",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "i3x66qyy",
+    "name": "Passport_Code",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ghk3ht54",
+    "name": "Valid",
+    "type": "bool",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {}
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "w3m2jabv",
+    "name": "Document_Photos",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [],
+      "thumbs": [],
+      "maxSelect": 99,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
