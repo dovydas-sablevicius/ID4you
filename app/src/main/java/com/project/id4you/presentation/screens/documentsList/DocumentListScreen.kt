fa23d5e4870575422221bb018ff1b9df214030bd
Dovydas Šablevičius <dovydas.sablevicius.it@gmail.com>
2024-04-21 23:40:56 +0300

    ID-77 update column height

--- app/src/main/java/com/project/id4you/presentation/screens/documentsList/DocumentListScreen.kt
+++ app/src/main/java/com/project/id4you/presentation/screens/documentsList/DocumentListScreen.kt
@@ -40,7 +40,7 @@ fun DocumentsListScreen(
         LazyColumn(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(600.dp)
+                .height(550.dp)
                 .testTag(TestTags.DOCUMENT_LIST),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
