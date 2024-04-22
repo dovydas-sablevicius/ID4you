282397a93854d7c54e49f5671c088132034527f7
Kaseriox <88597455+Kaseriox@users.noreply.github.com>
2024-04-23 01:02:17 +0300

    ID-83 Add name and surname fields to document details (#40)
    * ID-83 Added Name, Surname, Personal Code to detail screen, Fixed until Date to only show YYYY-MM-DD, fixed QR code button getting pushed out by big images
    
    * ID-83 Suppress MagicNumber lint error

--- app/src/main/java/com/project/id4you/presentation/screens/documentDetail/DocumentDetailScreen.kt
+++ app/src/main/java/com/project/id4you/presentation/screens/documentDetail/DocumentDetailScreen.kt
@@ -3,19 +3,18 @@ package com.project.id4you.presentation.screens.documentDetail
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.project.id4you.common.Constants
 import com.project.id4you.data.repository.model.Document
 import com.project.id4you.presentation.components.ButtonComponent
+import com.project.id4you.presentation.components.LoadingIndicator
 import com.project.id4you.presentation.components.text.TextComponent
 import com.project.id4you.presentation.components.text.TextType
 import com.project.id4you.presentation.screens.documentDetail.components.DocumentDetailScreenHeader
@@ -50,7 +49,7 @@ private fun LoadingScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        TextComponent(labelText = "Loading...", textType = TextType.HEADER)
+        LoadingIndicator()
     }
 }
 
@@ -65,6 +64,7 @@ private fun ErrorScreen(errorMessage: String) {
     }
 }
 
+@Suppress("MagicNumber")
 @Composable
 private fun SuccessScreen(
     onNavigateToDocumentQrScreen: (String) -> Unit,
@@ -72,8 +72,8 @@ private fun SuccessScreen(
 ) {
     val imageFetchUrl: String =
         Constants.BASE_URL + "/api/files/" + document.collectionId + "/" + document.id + "/"
-    val configuration = LocalConfiguration.current
-    val screenHeight = configuration.screenHeightDp.dp
+    val columnWeight: Float = 5f
+    val buttonWeight: Float = 1f
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -86,12 +86,12 @@ private fun SuccessScreen(
         )
         Column(
             modifier = Modifier
-                .padding(14.dp),
+                .padding(14.dp)
+                .weight(columnWeight),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             ImageSection(document, imageFetchUrl)
             InformationSection(
-                modifier = Modifier.heightIn(0.dp, screenHeight / 2),
                 document = document
             )
             StatusText(valid = document.valid)
@@ -102,7 +102,8 @@ private fun SuccessScreen(
             buttonColor = Color.Blue,
             modifier = Modifier
                 .width(350.dp)
-                .padding(bottom = 6.dp),
+                .padding(bottom = 6.dp)
+                .weight(buttonWeight, false),
             method = {
                 onNavigateToDocumentQrScreen(document.id)
             }
